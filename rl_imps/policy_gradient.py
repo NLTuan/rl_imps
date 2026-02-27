fa44f8757ff326ca741ba4a944c44ec51f6143b0
Le Tuan Huy Nguyen <lthnguyen27@gmail.com>
2026-02-27 09:55:39 -0500

    batched vanilla policy gradient

--- rl_imps/policy_gradient.py
+++ rl_imps/policy_gradient.py
@@ -23,7 +23,7 @@ class PolicyGradientModel(nn.Module):
         return self.fc3(h2)
         
 
-env = gym.make('CartPole-v1', render_mode="human")
+env = gym.make('CartPole-v1', render_mode="rgb_array")
 
 obs_dim = env.observation_space.shape[0]
 act_dim = env.action_space.n
@@ -31,50 +31,63 @@ policy = PolicyGradientModel(obs_dim, act_dim)
 
 obs, info = env.reset()
 
-n_steps = 10000
+n_steps = 100000
 
 gamma = 0.99
 
 step_count = 0
 
+n_eps_per_batch = 32
+
 lr = 3e-4
 opt = optim.AdamW(policy.parameters(), lr=lr)
 while step_count < n_steps:
-    terminated, truncated = False, False
+
+    if step_count >= 90000:
+        env = gym.make('CartPole-v1', render_mode="human")
+        obs, info = env.reset()
 
     states = []
-    rewards = []
     
     act_log_probs = []
-    while not (terminated or truncated) and step_count < n_steps:
-        action_dist = Categorical(logits = policy(tensor(obs)))
-        action = action_dist.sample()
-        act_log_probs.append(action_dist.log_prob(action))
-                
-        obs, reward, terminated, truncated, info = env.step(action.item())
-        states.append(obs)
-        rewards.append(reward)
-        step_count += 1
-    
-    
-    cum_rewards = copy.deepcopy(rewards)
-    for i in range(len(rewards)):
-        if i != 0:
-            cum_rewards[-(i+1)] = gamma * cum_rewards[-i] + rewards[-(i+1)]
+    total_cum_rewards = []
+    for i in range(n_eps_per_batch):
+        terminated, truncated = False, False
+        ep_rewards = []
 
-    # cum_rewards = list(reversed(cum_rewards))
-    act_log_probs = torch.stack(act_log_probs)
-    cum_rewards = torch.tensor(cum_rewards, dtype=torch.float32)
+        while not (terminated or truncated) and step_count < n_steps :
+            action_dist = Categorical(logits = policy(tensor(obs)))
+            action = action_dist.sample()
+            act_log_probs.append(action_dist.log_prob(action))
+                    
+            obs, reward, terminated, truncated, info = env.step(action.item())
+            states.append(obs)
+            ep_rewards.append(reward)
+            step_count += 1
+            
+        cum_rewards = copy.deepcopy(ep_rewards)
+        for i in range(len(ep_rewards)):
+            if i != 0:
+                cum_rewards[-(i+1)] = gamma * cum_rewards[-i] + ep_rewards[-(i+1)]
+        
+        total_cum_rewards += cum_rewards
+        
+        if step_count >= n_steps:
+            break
+        obs, info = env.reset()
+
     
-    cum_rewards = (cum_rewards - cum_rewards.mean()) / (cum_rewards.std() + 1e-8)
-    loss = -(act_log_probs * cum_rewards).mean()
+    act_log_probs = torch.stack(act_log_probs)
+    total_cum_rewards = torch.tensor(total_cum_rewards, dtype=torch.float32)
+    
+    total_cum_rewards = (total_cum_rewards - total_cum_rewards.mean()) / (total_cum_rewards.std() + 1e-8)
+    loss = -(act_log_probs * total_cum_rewards).mean()
     
     loss.backward()
     opt.step()
     opt.zero_grad()
     # import pdb; pdb.set_trace()
     print(loss)
-    obs, info = env.reset()
     
     
     
