f866b6a10c66025a3b61ae9620e30278f295f1ec
Le Tuan Huy Nguyen <lthnguyen27@gmail.com>
2026-03-02 16:57:07 -0500

    gae implementation

--- rl_imps/gae.py
+++ rl_imps/gae.py
@@ -40,12 +40,13 @@ obs, info = env.reset()
 n_steps = 100000
 
 gamma = 0.99
+lam = 0.99
 
 step_count = 0
 
 n_eps_per_batch = 8
 
-lr = 6e-3
+lr = 6e-2
 lr_baseline = 6e-4
 opt = optim.AdamW(policy.parameters(), lr=lr)
 opt_baseline = optim.AdamW(baseline.parameters(), lr=lr_baseline)
@@ -59,20 +60,25 @@ while step_count < n_steps:
                 viz = True
                 
     act_log_probs = []
+    
     total_cum_rewards = []
-    observations = []
+    
+    predicted_values = torch.tensor([], dtype=torch.float32)
+    advantages = torch.tensor([], dtype=torch.float32)
     
     eps_lens = []
     for i in range(n_eps_per_batch):
         terminated, truncated = False, False
         ep_rewards = []
+        
+        ep_obs = []
 
         step_before = step_count
         while not (terminated or truncated) and step_count < n_steps :
             action_dist = Categorical(logits = policy(tensor(obs, dtype=torch.float32)))
             action = action_dist.sample()
             act_log_probs.append(action_dist.log_prob(action))
-            observations.append(obs)
+            ep_obs.append(obs)
                     
             obs, reward, terminated, truncated, info = env.step(action.item())
             
@@ -82,18 +88,36 @@ while step_count < n_steps:
                 ep_rewards.append(reward)
 
             step_count += 1
-            
-            
         
-        eps_lens.append(step_count - step_before)
+        ep_len = step_count - step_before
+        eps_lens.append(ep_len)
+        
+        ep_obs = torch.tensor(ep_obs, dtype=torch.float32)
         
         cum_rewards = ep_rewards[:]
-        for j in range(len(ep_rewards)):
-            if j != 0:
-                cum_rewards[-(j+1)] = gamma * cum_rewards[-j] + ep_rewards[-(j+1)]
+        for i in reversed(range(len(cum_rewards) - 1)):
+            cum_rewards[i] = cum_rewards[i + 1] * gamma + ep_rewards[i]
         
         total_cum_rewards += cum_rewards
         
+        ep_rewards = torch.tensor(ep_rewards, dtype=torch.float32)
+        
+        eps_values = baseline(ep_obs).squeeze()
+        
+        predicted_values = torch.cat((predicted_values, eps_values))
+        
+        next_values = torch.cat((eps_values[1:], torch.tensor([0.0])))
+        
+        deltas = gamma * next_values + ep_rewards - eps_values 
+    
+        eps_advantage = torch.zeros(ep_len)
+        gae = 0
+        
+        for i in reversed(range(ep_len)):
+            gae = deltas[i] + gamma * lam * gae
+            eps_advantage[i] = gae
+        
+        advantages = torch.cat((advantages, eps_advantage))
         if step_count >= n_steps:
             break
         obs, info = env.reset()
@@ -101,14 +125,13 @@ while step_count < n_steps:
     
     
     act_log_probs = torch.stack(act_log_probs)
+    
     total_cum_rewards = torch.tensor(total_cum_rewards, dtype=torch.float32)
     
-    baseline_values = baseline(torch.tensor(observations, dtype=torch.float32)).squeeze()
-
-    loss_baseline = F.mse_loss(baseline_values, total_cum_rewards)
-
-    total_cum_rewards = (total_cum_rewards - total_cum_rewards.mean()) / (total_cum_rewards.std() + 1e-8)
-    loss = -(act_log_probs * (total_cum_rewards - baseline_values.detach())).mean()
+    loss_baseline = F.mse_loss(predicted_values, total_cum_rewards)
+    
+    advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+    loss = -(act_log_probs * advantages.detach()).mean()
 
     opt.zero_grad()
     loss.backward()
